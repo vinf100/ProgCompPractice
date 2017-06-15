4825d8ca04a817d40ebf72fdb57f737c7daaea5e
Brian Lee <briancmlee23@gmail.com>
2017-06-15 20:50:40 +1000

    Fix conditionals

--- humantime.py
+++ humantime.py
@@ -1,40 +1,43 @@
 time_input = input("enter robot time:")
 
 
-def set_hour_suffix(hour):
-    if hour == 12:
-        return " noon"
-    elif hour == 0:
-        return " midnight"
-    elif hour > 12:
-        return "am"
-    return "pm"
-
-
-def set_minute_suffix(minute):
-    if minute <= 30:
-        return " past"
-    elif minute > 30:
-        return " to"
-
-
-def set_minute_interval(minute):
-    if minute % 15 == 0:
-        if minute == 30:
-            return " half"
-        return " a quarter"
-    elif minute == 1:
-        return " a"
-    return " {} minutes".format(minute)
-
-
 def robot_to_human(robot_time):
     hour = int(robot_time[0:2])
     minute = int(robot_time[2:4])
 
-    hour_suffix = set_hour_suffix(hour)
-    minute_suffix = set_minute_suffix(minute)
-    minute_interval = set_minute_interval(minute)
+    minute_suffix = ""
+    minute_interval = ""
+
+    if hour >= 12:
+        hour_suffix = "pm"
+        hour -= 12
+    else:
+        minute_suffix = "am"
+
+    if hour % 12 == 0:
+        if hour == 12:
+            hour_suffix = " noon"
+        else:
+            hour_suffix = " midnight"
+            hour += 12
+
+    if minute > 0:
+        # set past
+        if minute <= 30:
+            minute_suffix = " past"
+        elif minute > 30:
+            minute_suffix = " to"
+            minute -= 30
+
+        # set minute interval
+        if minute == 30:
+            minute_interval = " half"
+        elif minute == 15:
+            minute_interval = " a quarter"
+        elif minute == 1:
+            minute_interval = " a"
+        else:
+            minute_interval = " {} minutes".format(minute)
 
     return "{} is{}{} {}{}".format(robot_time, minute_interval, minute_suffix, hour, hour_suffix)
 

e4880ea9c44136e4690bcc80642a0cb41bf6877d
Brian Lee <briancmlee23@gmail.com>
2017-06-15 19:47:47 +1000

    Makehumantime.py readable

--- humantime.py
+++ humantime.py
@@ -1,38 +1,42 @@
-input = raw_input("enter robot time:")
+time_input = input("enter robot time:")
 
-def hour_suffix(hour):
+
+def set_hour_suffix(hour):
     if hour == 12:
-        return "noon"
+        return " noon"
     elif hour == 0:
-        return "midnight"
+        return " midnight"
     elif hour > 12:
         return "am"
     return "pm"
 
-def minute_suffix(minute):
-    if minute <= 30:
-        return "past"
-    elif minute > 30:
-        return "to"
 
-def minute_interval(minute):
+def set_minute_suffix(minute):
+    if minute <= 30:
+        return " past"
+    elif minute > 30:
+        return " to"
+
+
+def set_minute_interval(minute):
     if minute % 15 == 0:
         if minute == 30:
-            return "half"
-        return "a quarter"
+            return " half"
+        return " a quarter"
     elif minute == 1:
         return " a"
-    return minute + "minutes"
+    return " {} minutes".format(minute)
 
 
 def robot_to_human(robot_time):
     hour = int(robot_time[0:2])
     minute = int(robot_time[2:4])
 
-    hour_suffix = hour_suffix(hour)
-    minute_suffix = minute_suffix(minute)
-    minute_interval = minute_interval(minute)
+    hour_suffix = set_hour_suffix(hour)
+    minute_suffix = set_minute_suffix(minute)
+    minute_interval = set_minute_interval(minute)
 
-    return "%s is %s %s %s%s" %(robot, str(minute_interval), str(minute_suffix), robot_hour, am_pm)
+    return "{} is{}{} {}{}".format(robot_time, minute_interval, minute_suffix, hour, hour_suffix)
 
-print(robot_to_human(input))
+
+print(robot_to_human(time_input))
